0d0aaebcd64311c16bb0aa2fdef4f44d463d16f5
ibmaciel <ibmaciel@users.noreply.github.com>
2018-12-15 00:20:25 -0200

    Finish

---
+++ Customising UI with Scriptable Objects/Assets/Scripts/FlexibleUIButton.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+[RequireComponent(typeof(Button))]
+public class FlexibleUIButton : FlexibleUI
+{
+    public enum ButtonType
+    {
+        Default,
+        Confirm,
+        Decline,
+        Warning
+    }
+
+
+    Button button;
+    Image icon;
+    Image image;
+
+    public ButtonType buttonType;
+
+    protected override void OnSkinUI()
+    {
+
+        base.OnSkinUI();
+
+        image = GetComponent<Image>();
+        icon = transform.Find("Icon").GetComponent<Image>();
+        button = GetComponent<Button>();
+
+        button.transition = Selectable.Transition.SpriteSwap;
+        button.targetGraphic = image;
+
+        image.sprite = skinData.buttonSprite;
+        image.type = Image.Type.Sliced;
+        button.spriteState = skinData.buttonSpriteState;
+
+        switch (buttonType)
+        {
+            case ButtonType.Confirm:
+                image.color = skinData.confirmColor;
+                icon.sprite = skinData.confirmIcon;
+                break;
+            case ButtonType.Decline:
+                image.color = skinData.declineColor;
+                icon.sprite = skinData.declineIcon;
+                break;
+            case ButtonType.Default:
+                image.color = skinData.defaultColor;
+                icon.sprite = skinData.defaultIcon;
+                break;
+            case ButtonType.Warning:
+                image.color = skinData.warningColor;
+                icon.sprite = skinData.warningIcon;
+                break;
+        }
+    }
+}
